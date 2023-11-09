dec62e83982b39c04702b64158e317a4e0da5992
Paolo Galli <paolo.galli7@studenti.unimi.it>
2023-11-09 18:16:20 +0100

    Simplify PostgresAppState::new function

--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -18,10 +18,8 @@ impl AppState<Postgres> for PostgresAppState {
 // Public impl
 impl PostgresAppState {
     pub async fn new(max_connections: u32, db_connection_string: &str) -> Result<PostgresAppState, sqlx::Error> {
-        match create_postgres_pool(max_connections, db_connection_string).await {
-            Ok(pool) => Ok(PostgresAppState { db: pool }),
-            Err(e) => Err(e)
-        }
+        let pool = create_postgres_pool(max_connections, db_connection_string).await?;
+        Ok(PostgresAppState { db: pool })
     }
 }
 
