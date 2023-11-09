07e51e63ad30841c846e61d5a6c2e6e3e5a56fb1
Paolo Galli <paolo.galli7@studenti.unimi.it>
2023-11-09 16:09:49 +0100

    Add migration script and connection to posgrtes database

---
+++ migrations/20231109101113_create_tables.sql
@@ -0,0 +1,27 @@
+-- Add migration script here
+CREATE TABLE buyer (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    created_at timestamp NOT NULL
+);
+
+CREATE TABLE event (
+    id SERIAL PRIMARY KEY,
+    description VARCHAR(1024) NOT NULL,
+    place VARCHAR(255) NOT NULL,
+    available_seats INTEGER NOT NULL,
+    price MONEY NOT NULL,
+    date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE ticket (
+    buyer_id INTEGER NOT NULL,
+    event_id INTEGER NOT NULL,
+    PRIMARY KEY (buyer_id, event_id),
+
+    CONSTRAINT buyer_id FOREIGN KEY(buyer_id) REFERENCES buyer(id),
+    CONSTRAINT event_id FOREIGN KEY(event_id) REFERENCES event(id)
+);
+
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 mod services;
 
-use actix_web::{App, HttpServer};
+use actix_web::{web::Data, App, HttpServer};
 use dotenv::dotenv;
 use services::status;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
+pub struct AppState {
+    db: Pool<Postgres>,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+
+    let db_connection_string = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_connection_string)
+        .await
+        .expect("Error building a connection pool");
 
     let address = std::env::var("ADDRESS").expect("ADDRESS must be set");
     let port_string = std::env::var("PORT").expect("PORT must be set");
@@ -16,8 +27,12 @@ async fn main() -> std::io::Result<()> {
         .expect("PORT variable is not an int");
 
     println!("Starting the server...");
-    HttpServer::new(move || App::new().service(status))
-        .bind((address, port))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(AppState { db: pool.clone() }))
+            .service(status)
+    })
+    .bind((address, port))?
+    .run()
+    .await
 }
