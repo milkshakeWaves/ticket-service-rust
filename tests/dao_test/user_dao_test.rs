a1af2ade065e2bf1d9d1d8bc3b6b847a1b016413
Paolo Galli <paolo.galli7@studenti.unimi.it>
2023-11-13 17:32:00 +0100

    Add user dao integration tests

--- tests/dao_test/user_dao_test.rs
+++ tests/dao_test/user_dao_test.rs
@@ -1,19 +1,77 @@
-use ticket_service_rust::models::CreateUserBody;
-
 use super::init_db_context;
 
 #[actix_rt::test]
 async fn add_user_returns_1() -> Result<(), sqlx::Error> {
     let db = init_db_context().await;
-    let user = CreateUserBody {
-        email: String::from("gigi@email.com"),
-        password: String::from("fake-password"),
-    };
+    let email = "gigi@email.com";
+    let password = "fake-password";
 
-    let result = db.users.add_user(&user.email, &user.password).await;
+    let result = db.users.add_user(email, password).await;
 
     assert!(result.is_ok());
     assert_eq!(1, result.unwrap());
 
+    let del_res = db.users.delete_user(email).await?;
+    assert_eq!(1, del_res);
+
     Ok(())
+}
+
+#[actix_rt::test]
+async fn add_user_returns_err_when_duplicate_email_is_added(
+) -> Result<(), sqlx::Error> {
+    let db = init_db_context().await;
+
+    let email = "bob@email.com";
+    let password = "password";
+
+    let result = db.users.add_user(email, password).await?;
+    assert_eq!(1, result);
+
+    let result = db.users.add_user(email, password).await;
+    assert!(result.is_err());
+
+    let del_res = db.users.delete_user(email).await?;
+    assert_eq!(1, del_res);
+
+    Ok(())
+}
+
+#[actix_rt::test]
+async fn get_user_by_email_returns_error_when_user_does_not_exist() -> () {
+    let db = init_db_context().await;
+
+    let result = db.users.get_user_by_email("non_existing@email.com").await;
+    assert!(result.is_ok_and(|user_opt| user_opt.is_none()));
+}
+
+#[actix_rt::test]
+async fn get_user_by_email_returns_user_when_user_exists() -> Result<(), sqlx::Error> {
+    let db = init_db_context().await;
+    
+    let email = "email@email.com";
+    let password = "password";
+
+    let _ = db.users.add_user(email, password).await?;
+
+    let result = db.users.get_user_by_email(email).await;
+    assert!(result.is_ok());
+    let result = result.unwrap();
+    assert!(result.is_some_and(|r| email == r.email));
+
+    let del_res = db.users.delete_user(email).await?;
+    assert_eq!(1, del_res);
+
+    Ok(())
+}
+
+#[actix_rt::test]
+async fn delete_user_returns_0_when_user_does_not_exist() -> () {
+    let db = init_db_context().await;
+    let email = "email@email.com";
+
+    let result = db.users.delete_user(email).await;
+    assert!(result.is_ok());
+    let result = result.unwrap();
+    assert_eq!(0, result);
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,7 +1,7 @@
 use ticket_service_rust::dao::Database;
 
 async fn init_db_context() -> Database<'static> {
-    let db_connection_string = "postgresql://ticket_service:ticket_service@localhost:5432/ticket_app";
+    let db_connection_string = "postgresql://ticket_service:ticket_service@localhost:5432/ticket_app_test";
     Database::new(db_connection_string).await
 }
 
