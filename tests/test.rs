95400d14e6e79d71f82aa81959495511f49d5ba7
Paolo Galli <paolo.galli7@studenti.unimi.it>
2023-11-14 18:19:28 +0100

    Creating test db connection string in test

--- tests/test.rs
+++ tests/test.rs
@@ -4,8 +4,12 @@ use dotenv::dotenv;
 async fn init_db_context() -> Database<'static> {
     dotenv().ok();
     let db_connection_string = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
-    Database::new(&db_connection_string).await
+    let test_db_conn = if !db_connection_string.contains("test") {
+        format!("{}_test", db_connection_string)
+    } else {
+        db_connection_string
+    };
+    Database::new(&test_db_conn).await
 }
 
 #[cfg(test)]
