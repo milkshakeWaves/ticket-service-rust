15f38dbd49b837a68510c58b9c7eb629bc3eccf3
Paolo Galli <paolo.galli7@studenti.unimi.it>
2023-11-14 18:13:03 +0100

    Debug ci/cd

--- tests/test.rs
+++ tests/test.rs
@@ -5,7 +5,7 @@ async fn init_db_context() -> Database<'static> {
     dotenv().ok();
     let db_connection_string = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-    Database::new(&format!("{}_test", db_connection_string)).await
+    Database::new(&db_connection_string).await
 }
 
 #[cfg(test)]
