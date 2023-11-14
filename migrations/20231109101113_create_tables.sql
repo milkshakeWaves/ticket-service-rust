69daeca5986c6cc1e5f472ddb18e8eb359a41548
Paolo Galli <paolo.galli7@studenti.unimi.it>
2023-11-14 11:52:41 +0100

    Replace test db string with github secret

--- migrations/20231109101113_create_tables.sql
+++ migrations/20231109101113_create_tables.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS event (
     date TIMESTAMP NOT NULL
 );
 
-CREATE TABLE ticket (
+CREATE TABLE IF NOT EXISTS ticket (
     user_id INTEGER NOT NULL,
     event_id INTEGER NOT NULL,
     PRIMARY KEY (user_id, event_id),
