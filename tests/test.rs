7f861947f124fe5872d507131cbc62c418b47b60
Paolo Galli <paolo.galli7@studenti.unimi.it>
2023-11-14 12:20:10 +0100

    Fix github action database string

--- tests/test.rs
+++ tests/test.rs
@@ -3,7 +3,7 @@ use dotenv::dotenv;
 
 async fn init_db_context() -> Database<'static> {
     dotenv().ok();
-    let db_connection_string = std::env::var("TEST_DATABASE_URL").expect("DATABASE_URL must be set");
+    let db_connection_string = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     Database::new(&db_connection_string).await
 }
